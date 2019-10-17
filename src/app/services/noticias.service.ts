7abb2977495f7d9e7ba98b32d93776b2033d4aec
= <=>
2019-10-17 19:52:50 +0200

    modulos

--- src/app/services/noticias.service.ts
+++ src/app/services/noticias.service.ts
@@ -10,6 +10,6 @@ export class NoticiasService {
   constructor(private http: HttpClient) { }
 
   getTopHeadlines(){
-    return this.http.get<RespuestaTopHeadlines>('https://newsapi.org/v2/everything?q=bitcoin&from=2019-09-16&sortBy=publishedAt&apiKey=ad139d2f1ff44d528eb44bbe77e5889a');
+    return this.http.get<RespuestaTopHeadlines>('https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=ad139d2f1ff44d528eb44bbe77e5889a');
   }
 }
